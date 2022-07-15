42540468d3fafc2ead685aa74c16a2d3db88c914
ThomasCza <czarnecki.thomas@orange.fr>
2022-07-15 22:44:11 +0200

    Modifications
    ok

---
+++ src/Controller/DBTestController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Page;
+use App\Entity\Tag;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DbTestController extends AbstractController
+{
+    #[Route('/db/test', name: 'app_db_test')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        // récupération du repository des catégories
+        $repository = $doctrine->getRepository(Category::class);
+        // récupération de la liste complète de toutes les catégories
+        $categories = $repository->findAll();
+        // inspection de la liste
+        dump($categories);
+
+        // récupération du repository des tags
+        $repository = $doctrine->getRepository(Tag::class);
+        // récupération de la liste complète de tous les tags
+        $tags = $repository->findAll();
+        // inspection de la liste
+        dump($tags);
+
+        // récupération du repository des articles
+        $repository = $doctrine->getRepository(Article::class);
+        // récupération de la liste complète de tous les articles
+        $articles = $repository->findAll();
+
+        foreach ($articles as $article) {
+            // inspection de l'article
+            dump($article);
+
+            // récupération des tags de l'article
+            $tags = $article->getTags();
+
+            foreach ($tags as $tag) {
+                // inspection du tag
+                dump($tag);
+            }
+        }
+
+        // récupération du repository des pages
+        $repository = $doctrine->getRepository(Page::class);
+        // récupération de la liste complète de toutes les pages
+        $pages = $repository->findAll();
+        // inspections de la liste des pages
+        dump($pages);
+
+        exit();
+    }
+}
